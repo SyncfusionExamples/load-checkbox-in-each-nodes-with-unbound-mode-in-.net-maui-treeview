eeab37defa8f7e73a8173dc560e6893800652740
AneesFathimaS <aneesfathima.shajahan@syncfusion.com>
2024-02-12 12:56:13 +0530

    866552-Sample for checkbox feature in unbound mode SfTreeView.

---
+++ UnboundModeCheckbox/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UnboundModeCheckbox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
